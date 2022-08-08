66bdfd181ce5ec9d7af52ff573b8e633ba9836f6
dipesh <jaswanidipesh8@gmail.com>
2022-08-08 19:26:25 +0530

    some bugs fixed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import {useEffect} from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Landing from './pages/Landing'
 import Screening from './pages/Screening'
-<<<<<<< HEAD
 import Header from './components/Header'
 import Footer from './components/Footer'
-=======
-import Navbar from './components/Navbar'
-import Footer from './pages/Footer'
->>>>>>> c3cdc118d2643dd6a7a5faf3760480d1efd093b2
+
 
 const App = ()=> {
   return (
