667a8e9444b4b23f30e27bb5c2b2c2c0ce39d77e
Shrivats <shrivatskrishna25@gmail.com>
2022-08-08 09:21:19 +0530

    "Landing page"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router-dom'
 import Landing from './pages/Landing'
 import Screening from './pages/Screening'
 import Navbar from './components/Navbar'
+import Footer from './pages/Footer'
 
 const App = ()=> {
   return (
@@ -9,9 +10,12 @@ const App = ()=> {
      <Navbar />
       <Routes>
         <Route path="/" exact element={<Landing />}></Route>
+        <Route path="/home" exact element={<Landing/>}></Route>
         <Route path="/screening" exact element={<Screening />}></Route>
       </Routes>
+      <Footer/>
     </div>
+
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const NavBar = ()=>{
         {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
         <Nav className="container-fluid">
         <Link  className="navbar-brand" to="/" >
-           <img src="" alt="Logo" className="img-fluid" style={{'maxWdth': '80%','maxHeight':'90%'}}/>
+           <img src="images/newLogo.png" alt="Logo" className="img-fluid" style={{'maxWdth': '80%','maxHeight':'90%'}}/>
         </Link>
           <Nav.Link as={Link} to ={"/home"} >Home</Nav.Link>
           <Nav.Link as={Link} to ={"/screening"}>Screening</Nav.Link>
