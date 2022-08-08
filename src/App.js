3f6833b61950a27b33bc663e5ac1e4fefd012026
RaveenaBhasin <raveenabhasin15@gmail.com>
2022-08-08 21:59:53 +0530

    feat: add signup and login page UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Landing from './pages/Landing'
 import Screening from './pages/Screening'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 
 const App = ()=> {
@@ -14,6 +16,8 @@ const App = ()=> {
         <Route path="/" exact element={<Landing />}></Route>
         <Route path="/home" exact element={<Landing/>}></Route>
         <Route path="/screening" exact element={<Screening />}></Route>
+        <Route path="/signup" exact element={<Signup />}></Route>
+        <Route path="/login_pg" exact element={<Login />}></Route>
       </Routes>
       <Footer/>
     </div>
