bfb1c19f8447ab7f5d325b0d114bd6f7d52e4d71
Prabhanshu Rawat <rawatprabhanshu123@gmail.com>
2022-10-23 00:44:36 +0530

    added-text-decor

--- src/pages/assesment/Assessment.jsx
+++ src/pages/assesment/Assessment.jsx
@@ -6,10 +6,10 @@ const Assessment = () => {
         <div style={{ marginTop: '200px' }}>
             <h1>Assessment</h1>
             <ul type="circle" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <li className='mt-2'><NavLink to='/spell'><h3>Spell</h3></NavLink></li>
-                <li className='mt-2'> <NavLink to='/read'><h3>Reading</h3></NavLink></li>
-                <li className='mt-2'><NavLink to='/math'><h3>Math</h3></NavLink></li>
-                <li className='mt-2'><NavLink to='/imagequiz'><h3>ImageQuiz</h3></NavLink></li>
+                <li className='mt-2 text-decoration-none'><NavLink to='/spell'><h3>Spell</h3></NavLink></li>
+                <li className='mt-2 text-decoration-none'> <NavLink to='/read'><h3>Reading</h3></NavLink></li>
+                <li className='mt-2 text-decoration-none'><NavLink to='/math'><h3>Math</h3></NavLink></li>
+                <li className='mt-2 text-decoration-none'><NavLink to='/imagequiz'><h3>ImageQuiz</h3></NavLink></li>
             </ul>
         </div>
     );
