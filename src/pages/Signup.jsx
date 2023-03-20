e4090d5791893dc21ea0719b0b795789db1a632e
SanketUpadhyay01 <88078531+SanketUpadhyay01@users.noreply.github.com>
2023-03-21 04:05:05 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Auth.css"
+import "./SignUpDoc"
+import "./SignUpUser"
 import {Link} from "react-router-dom";
 
 
@@ -19,21 +21,17 @@ class Signup extends React.Component {
       <div className="signupDiv" >
             <form className="signupForm" onSubmit={e=>this.props.onSignup(e,this.state)} >
                 <h3>Sign Up</h3>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input onChange={e=>this.setState({name : e.target.value})} type="text" className="form-control" placeholder="Name" />
+                <div className="signupMain">
+                <div>
+                    <Link to="/signup-doc"><button type="submit" className="btn btn-primary btn-block">I am a Doctor</button></Link>
                 </div>
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input onChange={e=>this.setState({email : e.target.value})} type="email" className="form-control" placeholder="Enter email" />
+                <div>
+                    
+                    <Link to="/signup-user"><button type="submit" className="btn btn-primary btn-block">I am a User</button></Link>
                 </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input onChange={e=>this.setState({password : e.target.value })} type="password" className="form-control" placeholder="Enter password" />
                 </div>
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                 <p className="forgot-password text-right">
-                    Already registered <Link to="/login_pg">sign in?</Link>
+                Already registered <Link to="/login_pg">sign in?</Link>
                 </p>
             </form>
             </div>
